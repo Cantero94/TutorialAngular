b6491b7a6dcfaa10f4a9f09d5f87af0fd4185d72
Cantero94 <jccanterogarcia@gmail.com>
2025-02-19 10:32:54 +0100

    angular material

--- src/app/components/card-angular-materials/card-angular-materials.component.ts
+++ src/app/components/card-angular-materials/card-angular-materials.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Renderer2, ElementRef } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-card-angular-materials',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './card-angular-materials.component.html',
   styleUrl: './card-angular-materials.component.css'
 })
@@ -14,5 +16,23 @@ export class CardAngularMaterialsComponent {
   @Input() misrc: string = '';
   @Input() mialt: string = '';
   @Input() texto: string = '';
+  @Input() avatar: string = '';
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit() {
+    const avatarElements = this.el.nativeElement.querySelectorAll('.example-header-image');
+    avatarElements.forEach((element: any) => {
+      this.renderer.setStyle(element, 'background-image', `url(${this.avatar})`);
+    });
+  }
+  
+
+  @Input() likes: number = 0; // Cambiar a number para manejar el contador
+
+  isLiked: boolean = false;
+    toggleLike() {
+    this.isLiked = !this.isLiked;
+    this.likes += this.isLiked ? 1 : -1;
+  }
 }
